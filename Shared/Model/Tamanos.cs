4d250422d6ad2ab818b236afa8a31e823a05bc19
HP <HP@DESKTOP-UVQHSF9>
2023-09-05 18:25:45 -0600

    Agregar archivos de proyecto.

---
+++ Shared/Model/Tamanos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaBlazor.Shared.Model
+{
+	public class Tamanos
+	{
+		public int Id { get; set; }
+		public string Tamano { get; set; }
+		public float Precio { get; set; }
+	}
+}
---
+++ Shared/Model/TipoMasa.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaBlazor.Shared.Model
+{
+	public class TipoMasa
+	{
+		public int Id { get; set; }
+		public string Tipo { get; set; }
+		public float Precio { get; set;}
+	}
+}
